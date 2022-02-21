91ca46e90fd1d1c457bde260672e434223f00913
mrzleo <mr.zleo10@gmail.com>
2022-02-21 17:47:00 +0800

    heap and heap-sort

---
+++ cpp/src/heap.h
@@ -0,0 +1,76 @@
+#include <vector>
+#include <iostream>
+
+using std::vector;
+
+template<class E>
+class Heap {
+private:
+  vector<E> &arr;
+  int _size;
+
+public:
+  Heap(vector<E> &arr) : arr(arr), _size(arr.size()) {
+	for (int i = arr.size(); i != -1; --i) {
+	  heapify(i);
+	}
+  }
+
+  int size();
+
+  void heapify(int index);
+
+  void swap(int i, int j);
+
+  vector<E> get_arr();
+
+  int *size_mut();
+};
+
+template<class E>
+int Heap<E>::size() { return _size; }
+
+template<class E>
+void Heap<E>::heapify(int index) {
+  while (true) {
+	auto max = index;
+	auto l = index * 2 + 1;
+	auto r = index * 2 + 2;
+	if (l < _size)
+	  max = l;
+	if (r < _size && arr[r] > arr[max])
+	  max = r;
+
+	// last one, recursion end
+	if (index == max)
+	  break;
+
+	if (arr[max] > arr[index])
+	  std::swap(arr[index], arr[max]);
+	index = max;
+  }
+}
+
+template<class E>
+void Heap<E>::swap(int i, int j) {
+  std::swap(arr[i], arr[j]);
+}
+template<class E>
+vector<E> Heap<E>::get_arr() {
+  return arr;
+}
+template<class E>
+int *Heap<E>::size_mut() {
+  return &_size;
+}
+
+template<class T>
+void heap_sort(vector<T> &arr) {
+  auto heap = Heap(arr);
+  auto size = heap.size() - 1;
+  for (auto i = size; i != 0; --i) {
+	heap.swap(0, i);
+	(*heap.size_mut())--;
+	heap.heapify(0);
+  }
+}
---
+++ cpp/test/heap_test.cpp
@@ -0,0 +1,64 @@
+#include "gtest/gtest.h"
+#include "../src/heap.h"
+#include <vector>
+
+GTEST_API_ int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
+TEST(reverse, t1) {
+  auto arr = std::vector<int>({4, 3, 2, 1, 0});
+  heap_sort(arr);
+  for (int i = 0; i < 5; ++i) {
+	EXPECT_EQ(i, arr[i]);
+  }
+}
+
+TEST(random, normalCase) {
+  auto arr = std::vector<int>({21, 1234, 24356, 7645, 123, 1354, 7467, 86, 123, 245, 345});
+  heap_sort(arr);
+  for (int i = 1; i < arr.size(); ++i) {
+	EXPECT_GE(arr[i], arr[i - 1]);
+  }
+}
+
+TEST(random, bigDataSet) {
+  const int BIG_DATA_SIZE = 100000000;
+  auto arr = std::vector<int>();
+  for (int i = 0; i < BIG_DATA_SIZE; ++i) {
+	arr.push_back(random());
+  }
+  heap_sort(arr);
+  for (int i = 1; i < arr.size(); ++i) {
+	EXPECT_GE(arr[i], arr[i - 1]);
+  }
+}
+
+TEST(lotsSameNum, bigDataSet) {
+  const int BIG_DATA_SIZE = 100000000;
+  auto arr = std::vector<int>();
+  for (int i = 0; i < BIG_DATA_SIZE; ++i) {
+	arr.push_back(random() % 100);
+  }
+  heap_sort(arr);
+  for (int i = 1; i < arr.size(); ++i) {
+	EXPECT_GE(arr[i], arr[i - 1]);
+  }
+}
+
+TEST(sorted, t1) {
+  auto arr = std::vector({1, 2, 3, 4, 5, 6});
+  heap_sort(arr);
+  for (int i = 1; i < arr.size(); ++i) {
+	EXPECT_GE(arr[i], arr[i - 1]);
+  }
+}
+
+TEST(lotsSameNum, t1) {
+  auto arr = std::vector{1, 1, 2, 3, 4, 5, 4, 1, 3, 2, 1, 1, 0, 1, 1, 1};
+  heap_sort(arr);
+  for (int i = 1; i < arr.size(); ++i) {
+	EXPECT_GE(arr[i], arr[i - 1]);
+  }
+}
