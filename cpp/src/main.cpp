b0e2e9ccdf9f547d8d7a09222dae160078bb35eb
mrzleo <mr.zleo10@gmail.com>
2022-02-23 11:30:05 +0800

    Implement quicksort and test

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,16 +1,25 @@
 #include "heap.hpp"
+#include "quicksort.hpp"
 
 int main() {
-  const int BIG_DATA_SIZE = 100000000;
+  const int BIG_DATA_SIZE = 10000000;
   auto arr = std::vector<int>();
-  for (auto i = BIG_DATA_SIZE; i > 0; --i) {
-    arr.push_back(i);
+  for (int i = 0; i < BIG_DATA_SIZE; ++i) {
+    //    arr.push_back(random() % 100);
+    arr.push_back(random());
   }
+  auto arr2 = arr;
   clock_t begin = clock();
   heap_sort(arr);
   clock_t end = clock();
-  std::cout << "total time: " << double(end - begin) / CLOCKS_PER_SEC
-            << std::endl;
+  std::cout << "total time for heap sort: "
+            << double(end - begin) / CLOCKS_PER_SEC << "s" << std::endl;
+
+  begin = clock();
+  quicksort2ways(arr2, 0, arr.size());
+  end = clock();
+  std::cout << "total time for quick sort: "
+            << double(end - begin) / CLOCKS_PER_SEC << "s" << std::endl;
 
   return 0;
 }
---
+++ cpp/src/quicksort.hpp
@@ -0,0 +1,85 @@
+#ifndef DATA_STRUCTURE_ALGORITHM_SRC_QUICKSORT_HPP
+#define DATA_STRUCTURE_ALGORITHM_SRC_QUICKSORT_HPP
+
+#include <vector>
+
+using std::vector;
+
+template <class T> T partition(vector<T> &arr, int l, int r) {
+  std::swap(arr[l], arr[(random() % (r - l)) + l]);
+  const int p = arr[l];
+  int j = l; // [l, j] stands for arr[j] <= p
+  for (int i = l + 1; i < r; ++i) {
+    if (arr[i] <= p) {
+      j++;
+      std::swap(arr[j], arr[i]);
+    }
+  }
+
+  std::swap(arr[l], arr[j]);
+  return j;
+}
+
+template <class T> T parition2(vector<T> &arr, int l, int r) {
+  std::swap(arr[l], arr[(random() % (r - l)) + l]);
+  const int p = arr[l];
+  int j = l + 1;
+  int k = r - 1;
+  while (true) {
+    while (j < r && arr[j] < p)
+      j++;
+    while (k > l && arr[k] > p)
+      k--;
+
+    if (j > k)
+      break;
+
+    std::swap(arr[j], arr[k]);
+    j++;
+    k--;
+  }
+
+  std::swap(arr[l], arr[k]);
+  return k;
+}
+
+/**
+ * can not handle identical element
+ */
+template <class T> void quicksort(vector<T> &arr, int l, int r) {
+  switch (r - l) {
+  case 0:
+  case 1:
+    return;
+  case 2:
+    if (arr[l] > arr[l + 1])
+      std::swap(arr[l], arr[l + 1]);
+    return;
+  default:
+    break;
+  }
+
+  int i = partition(arr, l, r);
+  quicksort(arr, l, i + 1);
+  quicksort(arr, i + 1, r);
+}
+
+template <class T> void quicksort2ways(vector<T> &arr, int l, int r) {
+  switch (r - l) {
+  case 0:
+  case 1:
+    return;
+  case 2:
+    if (arr[l] > arr[l + 1])
+      std::swap(arr[l], arr[l + 1]);
+    return;
+  default:
+    break;
+  }
+
+  int i = parition2(arr, l, r);
+  quicksort2ways(arr, l, i + 1);
+  quicksort2ways(arr, i + 1, r);
+}
+
+#endif // DATA_STRUCTURE_ALGORITHM_SRC_QUICKSORT_HPP
