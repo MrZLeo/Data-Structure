b78b4a4e35df6f208b48684b625f9709eb254533
mrzleo <mr.zleo10@gmail.com>
2022-06-08 11:28:41 +0800

    fix bug in matrix_chain

--- cpp/src/matrix_chain.cpp
+++ cpp/src/matrix_chain.cpp
@@ -1,20 +1,37 @@
 #include "matrix_chain.hpp"
 #include <cstdint>
+#include <iostream>
 #include <vector>
+
+void print_optimal(std::vector<std::vector<int>> &s, int i, int j)
+{
+    if (i == j) {
+        std::cout << "A" << i;
+        return;
+    }
+
+    std::cout << "(";
+    print_optimal(s, i, s[i][j]);
+    print_optimal(s, s[i][j] + 1, j);
+    std::cout << ")";
+}
 
 /**
  * @algo matrix_chain
  *
- * @argu p: each Ai = p_{i-1} * p_{i}
- *
- *
+ * @args p: each Ai = p_{i-1} * p_{i}
  */
 int matrix_chain(std::vector<int> &p)
 {
-    const int n = p.size();
+    // Ai = p_{i - 1} * p_{i}
+    // An = p_{n - 1} * p_{n}
+    // so p.size() == n + 1
+    const int n = p.size() - 1;
+
     // memo[i][j] indicates that A[i, j]'s lowest calculate times
     std::vector<std::vector<int>> memo
         = std::vector(n + 1, std::vector(n + 1, INT32_MAX));
+
     // solu[i][j] shows that which k should be choose so that
     // A[i, k] * A[k, j] can be best answer
     std::vector<std::vector<int>> solu
@@ -27,18 +44,29 @@ int matrix_chain(std::vector<int> &p)
     }
 
     // start from j - i == 1
+    // l means the size of matrix this turn will compute
+    // e.g. l == 2 means j - i + 1 == 2, i.e. m[i][j] contains 2 matrix
     for (int l = 2; l <= n; ++l) {
         for (int i = 1; i <= n - l + 1; ++i) {
-            int j = i + l - 1;
+            int j = i + l - 1; // j - i == l - 1
+            // i <= k < j
+            // find the minimum sulution by computing each k
+            // memo[i][k] and memo[k + 1][j] already calculted in the past
             for (int k = i; k < j; ++k) {
                 int q = memo[i][k] + memo[k + 1][j] + p[i - 1] * p[k] * p[j];
                 if (q < memo[i][j]) {
                     memo[i][j] = q;
+                    // if we need to find out the solution
+                    // rather than the minimum, we need to
+                    // store k in solu[i][j]
                     solu[i][j] = k;
                 }
             }
         }
     }
 
-    return memo[1][n - 1];
+    print_optimal(solu, 1, n);
+    std::cout << std::endl;
+
+    return memo[1][n];
 }
