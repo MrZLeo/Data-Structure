c5651ba74a6a7ec775f4dea9d33664397aa3f4d1
mrzleo <mr.zleo10@gmail.com>
2022-06-13 19:36:13 +0800

    add dp 647 Palindromic Substrings

---
+++ cpp/src/dp/palindromic_substrings_647.cpp
@@ -0,0 +1,52 @@
+#include <cctype>
+#include <cstdint>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int countSubstrings(string s)
+    {
+        const int n = s.size();
+        // memo[i][j] = str[i..j] is a palindromic substring
+        vector<vector<bool>> memo = vector(n + 1, vector(n + 1, false));
+
+        // res is the size of palindromic substring
+        int res = n;
+
+        // any one char can be a palindromic substring
+        for (int i = 1; i <= n; ++i)
+            memo[i][i] = true;
+
+        // we need to calculate by step
+        for (int l = 2; l <= n; ++l) {
+            for (int i = 1; i <= n - l + 1; ++i) {
+                int j = i + l - 1;
+
+                // j - i == 1 will means situation like memo[1][2], which will
+                // lead to i + 1 > j - 1, but in this situation, we just need
+                // to judge whether s[i-1] == s[j-1]
+                if ((j - i <= 1 || memo[i + 1][j - 1] == true)
+                    && s[i - 1] == s[j - 1]) {
+                    memo[i][j] = true;
+                    res++;
+                }
+            }
+        }
+
+        return res;
+    }
+};
+
+void test()
+{
+    cout << Solution().countSubstrings("aaa") << endl;
+}
